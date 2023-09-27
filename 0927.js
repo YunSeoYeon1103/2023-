f3f7a802c90ee5d45b713f3091afdf371e4a6392
YunSeoYeon1103 <133744295+YunSeoYeon1103@users.noreply.github.com>
2023-09-27 16:34:28 +0900

    9/27
    연산자
    -산술
    -관계
    -논리

---
+++ 0927.js
@@ -0,0 +1,22 @@
+console.log('my'+'cat');
+console.log('1'+2);
+console.log("ellie's \n\tbook");
+
+console.log(1+1);
+console.log(1-1);
+console.log(1/1);
+console.log(1*1);
+console.log(5%2); 
+console.log(2**3);
+
+console.log(10<6);
+console.log(10<=6);
+console.log(10>6);
+console.log(10>=6);
+
+const value1=true;
+const vaule2=4<2;
+console.log(value1||vaule2);
+console.log(value1&&vaule2);
+
+console.log(0==false);
