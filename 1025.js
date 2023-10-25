e0e42df457b4de13d0324728116f8b369076910d
YunSeoYeon1103 <133744295+YunSeoYeon1103@users.noreply.github.com>
2023-10-25 15:40:23 +0900

    Add files via upload

---
+++ 1025.js
@@ -0,0 +1,40 @@
+'use strict';
+
+//Array
+
+//1. Declaration
+const arr1 = new Array();
+const arr2 = [1, 2];
+
+//2. Index position
+const fruits = ['🍎', '🍌'];
+console.log(fruits);
+console.log(fruits.length);
+console.log(fruits[0]);
+console.log(fruits[fruits.length-1]);
+
+
+console.clear();
+//3. Looping over an array
+//print all fruits
+//a. for
+for (let i = 0; i<fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+//b. for of
+for(let fruit of fruits) {
+    console.log(fruit);
+}
+
+//c.forEach
+fruits.forEach((fruit) => console.log(fruit));
+
+//4. Addtion, deletion, copy
+//push: add an item to the end
+fruits.push('🍓', '🍑');
+console.log(fruits);
+
+//pop: remove an item from the end
+fruits.pop();
+console.log(fruits);
